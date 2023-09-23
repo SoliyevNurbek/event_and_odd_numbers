f9dae359ea60f8daea50e16d867537ad2816d3ec
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-23 10:41:48 +0500

    ex2

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,5 @@
-#Create a variable "number" and assign it a integer value.
-
-#Print 1 if the number is odd, otherwise print 0.
+number=int(25)
+if (number%2==1):
+    print("odd")
+else:
+    print("even")
