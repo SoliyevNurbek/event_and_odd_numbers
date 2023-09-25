f0f89b9bf0a749544b4a2c46d0729df69f61b2f8
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-25 16:12:42 +0500

    exx6

--- sum_odd.py
+++ sum_odd.py
@@ -4,3 +4,14 @@
 #Create a variable "sum_odd" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+x=1234
+k=0
+if ((x%10)%2==1):
+    k=k+(x%10)
+if(((x//10)%10)%2==1):
+    k=k+((x//10)%10)
+if(((x//100)%10)%2==1):
+    k=k+((x//100)%10)
+if((x//1000)%2==1):
+    k=k+(x//1000)
+print(k)
