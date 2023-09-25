d998aea4cb1a8c795570f3cd2646a584e52eb732
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-25 16:12:09 +0500

    exx5

--- sum_even.py
+++ sum_even.py
@@ -4,3 +4,14 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+x=1234
+k=0
+if ((x%10)%2==0):
+    k=k+(x%10)
+if(((x//10)%10)%2==0):
+    k=k+((x//10)%10)
+if(((x//100)%10)%2==0):
+    k=k+((x//100)%10)
+if((x//1000)%2==0):
+    k=k+(x//1000)
+print(k)
