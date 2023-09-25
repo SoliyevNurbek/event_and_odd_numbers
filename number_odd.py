03035267d92451c40e4cbc55106f5c47cad5bc40
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-25 16:11:45 +0500

    exx4

--- number_odd.py
+++ number_odd.py
@@ -2,3 +2,14 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+x=1234
+k=0
+if ((x%10)%2==1):
+    k=k+1
+if(((x//10)%10)%2==1):
+    k=k+1
+if(((x//100)%10)%2==1):
+    k=k+1
+if((x//1000)%2==1):
+    k=k+1
+print(k)
